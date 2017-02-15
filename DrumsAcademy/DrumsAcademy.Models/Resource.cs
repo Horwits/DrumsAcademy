99c26e21f43586b1fcc369abf046ce17735cab79
Daniel Nikolov <horwits1@hotmail.com>
2017-02-15 18:47:50 +0200

    Added Models library and solution folder

---
+++ DrumsAcademy/DrumsAcademy.Models/Resource.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using DrumsAcademy.Common.Enums;
+
+namespace DrumsAcademy.Models
+{
+    public class Resource
+    {
+        [Required]
+        public Guid Id { get; set; }
+
+        [Required]
+        public ResourceType Type { get; set; }
+
+        [Required]
+        public string Url { get; set; }
+
+        [Required]
+        [MinLength(50)]
+        [MaxLength(500)]
+        public string Title { get; set; }
+
+        [Required]
+        [MinLength(50)]
+        [MaxLength(500)]
+        public string Description { get; set; }
+
+        [Required]
+        public string Author { get; set; } // this has to be user ->
+    }
+}
