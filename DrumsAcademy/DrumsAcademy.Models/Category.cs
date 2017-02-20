df5b3e83a16764d74e307540a666fa2c90f77501
Daniel Nikolov <horwits1@hotmail.com>
2017-02-20 21:10:38 +0200

    Added db and seed

---
+++ DrumsAcademy/DrumsAcademy.Models/Category.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+using DrumsAcademy.Common.Enums;
+
+namespace DrumsAcademy.Models
+{
+    public class Category
+    {
+        public Category()
+        {
+            this.Resources = new HashSet<Resource>();
+        }
+
+        public Guid Id { get; set; }
+
+        [Required]
+        public CategoryType Type { get; set; }
+
+        public virtual IEnumerable<Resource> Resources { get; set; }
+    }
+}
--- DrumsAcademy/DrumsAcademy.Models/Resource.cs
+++ DrumsAcademy/DrumsAcademy.Models/Resource.cs
@@ -1,31 +1,39 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 using DrumsAcademy.Common.Enums;
 
 namespace DrumsAcademy.Models
 {
     public class Resource
     {
-        [Required]
         public Guid Id { get; set; }
+
+        public string Author { get; set; } // this has to be user ->
+
+        [Required]
+        [MinLength(10)]
+        [MaxLength(500)]
+        public string Description { get; set; }
+
+        [Required]
+        [MinLength(5)]
+        [MaxLength(50)]
+        [Index(IsUnique = true)]
+        public string Title { get; set; }
 
         [Required]
         public ResourceType Type { get; set; }
 
         [Required]
+        public LevelType Level { get; set; }
+
+        [Required]
         public string Url { get; set; }
 
-        [Required]
-        [MinLength(50)]
-        [MaxLength(500)]
-        public string Title { get; set; }
+        public virtual Category Category { get; set; }
 
-        [Required]
-        [MinLength(50)]
-        [MaxLength(500)]
-        public string Description { get; set; }
-
-        [Required]
-        public string Author { get; set; } // this has to be user ->
+        public Guid CategoryId { get; set; }
     }
 }
